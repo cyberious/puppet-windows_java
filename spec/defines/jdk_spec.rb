eae75974bb34eb54ee61c343dc7b762246dd8b2c
Travis Fields <tfields@commercehub.com>
2013-12-26 16:49:32 -0500

    updated Rspec-Puppet Configs and travis to have dependiencies

--- spec/defines/jdk_spec.rb
+++ spec/defines/jdk_spec.rb
@@ -1,15 +1,11 @@
 require 'spec_helper'
-
-describe 'windows_java::jdk', :type => :define do
-
-  let(:facts) {{:caller_module_name => 'spec',
-                :osfamily           => 'Windows',
-                :path               => 'C:/windows/system32',}}
-  define 'when deploying with defaults' do
+describe 'windows_java', :type => :define do
+  describe 'when deploying with defaults' do
     let(:title){'Install JDK'}
     it{
-      should contain_exec('Download-jdk-7u45-windows-x64.exe')
-      should contain_package('Java SE Development Kit 7 Update 45 (64-bit)')
+      #should contain_exec('Download-jdk-7u45-windows-x64.exe')
+      should contain_package('Java SE Development Kit 7 Update 45 (64-bit)').with_ensure('present')
+      #should contain_windows_env('JAVA_HOME')
     }
   end
 end
