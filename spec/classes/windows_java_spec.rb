5c60f3082b2f7783a587381150de532252930d01
Travis Fields <tfields@commercehub.com>
2014-01-14 17:27:10 -0500

    Format changes to meat some of puppet-lint testing

--- spec/classes/windows_java_spec.rb
+++ spec/classes/windows_java_spec.rb
@@ -1,18 +1,22 @@
 require 'spec_helper'
 require 'rspec-puppet'
-#require 'hiera-puppet-helper'
 
 describe 'windows_java' do
   include_context 'hieradata'
   let(:title) {'install jdk'}
-  let(:facts){{:operatingsystem => "windows"}}
+  let(:facts){{:operatingsystem => 'windows'}}
 
   it {
     should contain_pget('Download-jdk-7u45-windows-x64.exe').with({
-      'source' => "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-windows-x64.exe",
+      'source' => 'http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-windows-x64.exe',
       'target' => 'C:\\temp'
                                                                   })
-    should contain_package('Java SE Development Kit 7 Update 45 (64-bit)').with_ensure('present')
-    should contain_windows_env('JAVA_HOME').with_value("C:\\Program Files\\Java\\jdk1.7.0_45")
+    should contain_package(
+        'Java SE Development Kit 7 Update 45 (64-bit)').with({
+        'ensure' => 'present'
+        })
+    should contain_windows_env('JAVA_HOME').with({
+        'value'=>'C:\\Program Files\\Java\\jdk1.7.0_45'
+        })
   }
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,20 +2,20 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'hiera-puppet-helper/rspec'
 require 'hiera'
 
-
 def param_value(subject, type, title, param)
   subject.resource(type, title).send(:parameters)[param.to_sym]
 end
 
 RSpec.configure do |c|
   c.mock_framework = :rspec
-  c.module_path  = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/modules'))
+  baseFile = File.dirname(__FILE__)
+  c.module_path  = File.expand_path(File.join(baseFile, 'fixtures/modules'))
   # Using an empty site.pp file to avoid: https://github.com/rodjek/rspec-puppet/issues/15
-  c.manifest_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures/manifests'))
+  c.manifest_dir = File.expand_path(File.join(baseFile,'fixtures/manifests'))
 end
 
 
-data_path = File.expand_path(File.join(__FILE__, '..', 'fixtures','modules','hieradata'))
+data_path = File.expand_path(File.join(__FILE__,'..','fixtures','modules','hieradata'))
 shared_context "hieradata" do
   let(:hiera_config) do
     { :backends => ['rspec', 'yaml'],
